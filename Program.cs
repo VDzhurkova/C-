d94e44134c57538c8d57f3ccd91d4ac694662f20
VDzhurkova <velpr.vd@gmail.com>
2018-01-22 19:05:10 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,21 +10,28 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            double input1, input2;
+            double input1 = 0, input2 =0;
             char input3;
             double output = 0;
 
+            Console.WriteLine("Enter the sign + to add, - to substract, * to multiply, / to divide, ^ to raise to a power, i to invert, s for sine, c for cosine and t for tangent:");
+            input3 = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("Enter the first number:");
             input1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the second number:");
-            input2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the sign:");
-            input3 = Convert.ToChar(Console.ReadLine());
 
-            switch (input3) {
+            Console.WriteLine("Do you need to enter a second number (Y/N)");
+            string answer = Console.ReadLine();
+
+            if(answer == "Y") {
+                Console.WriteLine("Enter the second number:");
+                input2 = Convert.ToDouble(Console.ReadLine());
+            }
+
+            switch (input3)
+            {
 
                 case '+':
-                    output = addNumbers(input1,input2);
+                    output = addNumbers(input1, input2);
                     break;
                 case '-':
                     output = subNumbers(input1, input2);
@@ -35,12 +42,30 @@ namespace calculator
                 case '/':
                     output = divNumbers(input1, input2);
                     break;
-                default: break;
+                case '^':
+                    output = powerNumbers(Convert.ToInt32(input1), Convert.ToInt32(input2));
+                    break;
+                case 'i':
+                    output = invertNumbers(input1);
+                    break;
+                case 's':
+                    output = Math.Sin(Convert.ToInt32(input1));
+                    break;
+                case 'c':
+                    output = Math.Cos(Convert.ToInt32(input1)); ;
+                    break;
+                case 't':
+                    output = Math.Tan(Convert.ToInt32(input1)); ;
+                    break;
+                default:
+                    Console.WriteLine("Not a valid sign");
+                    break;
             }
             Console.WriteLine(output);
         }
 
-        public static double addNumbers(double firstNum, double secondNum) {
+        public static double addNumbers(double firstNum, double secondNum)
+        {
             return firstNum + secondNum;
         }
 
@@ -57,7 +82,8 @@ namespace calculator
         public static double divNumbers(double firstNum, double secondNum)
         {
             double result = 0;
-            if (secondNum == 0) {
+            if (secondNum == 0)
+            {
 
                 Console.WriteLine("Please enter number different than NULL");
                 secondNum = Convert.ToDouble(Console.ReadLine());
@@ -68,5 +94,16 @@ namespace calculator
             }
             return result;
         }
+
+        public static int powerNumbers(int value, int power) {
+
+            return Convert.ToInt32(Math.Pow(value, power));
+        }
+
+        public static double invertNumbers(double number) {
+            return (1 / number);
+        }
+
+
     }
 }
